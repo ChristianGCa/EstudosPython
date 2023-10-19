0e7c511b84eb8cf190782de426595597eb14fcd0
ChristianGCa <candelonichristian@gmail.com>
2023-10-19 18:38:24 -0300

    Fazendo o arquivo csv ser iterado corretamente

--- EI-l3-csv/lerCsv.py
+++ EI-l3-csv/lerCsv.py
@@ -1,9 +1,27 @@
 import csv
 import json
 
+# Abrindo o arquivo .csv
 arq = open("municipios_formatado.csv")
 
-dados = csv.reader(arq)
+# Armazenando o conteúdo do arquivo na variável input
+input = csv.reader(arq)
 
-for item in dados:
-    print(item)
+# Cria o objeto de gravação
+w = csv.writer(arq)
+
+# Pula a linha de cabeçalho
+next(arq, None)
+
+# Itera sobre as linhas do arquivo
+for row in arq:
+
+    # Cria um objeto JSON para o município
+    obj = {
+        "uf": row[0],
+        "cod_uf": row[1],
+        "cod_municipio": row[2],
+        "nome_municipio": row[3],
+        "populacao": row[4],
+    }
+    print(row)
