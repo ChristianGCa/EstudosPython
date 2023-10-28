46406aaed12616c3ac5d25d413d6ffed6da4cdff
ChristianGCa <candelonichristian@gmail.com>
2023-10-28 16:23:27 -0300

    Criando copias para testar

---
+++ EI-l3-csv/lerCsv copy.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+# Abrindo o arquivo .csv
+arq = open("municipios_formatado copy.csv")
+
+# Armazenando o conteúdo do arquivo na variável input
+input = csv.reader(arq)
+
+# Cria o objeto de gravação
+w = csv.writer(arq)
+
+# Pula a linha de cabeçalho
+next(arq, None)
+
+# Itera sobre as linhas do arquivo
+for row in arq:
+
+    # Cria um objeto JSON para o município
+    obj = {
+        "uf": row[0],
+        "cod_uf": row[1],
+        "cod_municipio": row[2],
+        "nome_municipio": row[3],
+        "populacao": row[4],
+    }
+    print(row)
