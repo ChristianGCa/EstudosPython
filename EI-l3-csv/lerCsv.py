e0b948ee9c262c9a7c7337dbe7b4e076d4ba8ed0
ChristianGCa <candelonichristian@gmail.com>
2023-10-28 17:45:51 -0300

    Finalizando o estudo independente 3

--- EI-l3-csv/lerCsv.py
+++ EI-l3-csv/lerCsv.py
@@ -7,21 +7,46 @@ arq = open("municipios_formatado.csv")
 # Armazenando o conteúdo do arquivo na variável input
 input = csv.reader(arq)
 
-# Cria o objeto de gravação
-w = csv.writer(arq)
-
 # Pula a linha de cabeçalho
-next(arq, None)
+next(input)
+
+ufs = []
 
 # Itera sobre as linhas do arquivo
-for row in arq:
+for row in input:
 
-    # Cria um objeto JSON para o município
-    obj = {
-        "uf": row[0],
-        "cod_uf": row[1],
-        "cod_municipio": row[2],
+    # Criando variáveis para guardar o código uf e a uf da linha
+    cod_uf = int(row[1])
+    uf_nome = row[0]
+
+    # Criando a variavel uf_atual vazia
+    uf_atual = None
+
+    # Verificando se a uf ja existe na lista de ufs
+    for uf in ufs:
+        if uf["cod_uf"] == cod_uf:
+            uf_atual = uf
+            break
+    
+    # Se a uf_atual está vazia, cria um dicionário para a uf
+    if uf_atual is None:
+        uf_atual = {
+            'uf': uf_nome,
+            'cod_uf': cod_uf,
+            'municipios': []
+        }
+
+        # Adicionando a uf atual na lista de UFs
+        ufs.append(uf_atual)
+
+    # Adiciona o municipio na lisa de minicipios da uf
+    uf_atual['municipios'].append({
+        "cod_municipio": int(row[2]),
         "nome_municipio": row[3],
-        "populacao": row[4],
-    }
-    print(row)
+        "populacao": int(row[4])
+    })
+
+    # Salvando em json
+    with open("saida_em_json.json", "w") as json_file:
+        json.dump(ufs, json_file, indent=2)
+
