4a3eaebfd5155cd6ce9f2b1dd7b6d42705e87180
ChristianGCa <candelonichristian@gmail.com>
2024-03-23 13:52:10 -0300

    Adicionando mais arquivos e fazendo merge entre tabelas

--- PI-BigData/visualizar.py
+++ PI-BigData/visualizar.py
@@ -1,12 +1,18 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-pars = pd.read_csv("/home/christian/Documentos/bases SIASUS/Bases-SIASUS/PARS2312.csv", encoding="ISO-8859-1")
+# Lendo os arquivos
+pars = pd.read_csv("/home/christian/Documentos/bases SIASUS/Bases-SIASUS/PARS2312.csv", encoding="ISO-8859-1", usecols=['PA_CIDPRI', 'PA_UFMUN', 'PA_CNPJCPF'])
+cid = pd.read_csv("/home/christian/Documentos/bases SIASUS/Bases-SIASUS/S_CID.csv", encoding="ISO-8859-1", usecols=['CD_COD', 'CD_DESCR'])
+mun = pd.read_csv("/home/christian/Documentos/bases SIASUS/Bases-SIASUS/tb_municip.csv", encoding="ISO-8859-1", usecols=['CO_MUNICIP', 'DS_NOME'])
+cpf_cnpj = pd.read_csv("/home/christian/Documentos/bases SIASUS/Bases-SIASUS/CADGERRS.csv", encoding="ISO-8859-1", usecols=['CPF_CNPJ', 'FANTASIA'])
 
-genero = pars["PA_SEXO"].value_counts()
-cpfCnpj = pars["PA_CNPJCPF"].value_counts()
+# Relacionando as colunas PA_CIDPRI e CD_COD das tabelas
+pars = pd.merge(pars, cid, left_on='PA_CIDPRI', right_on='CD_COD')
 
-cpf_e_genero = pd.concat([cpfCnpj, genero], axis=1)
-cpf_e_genero.columns = ["CPF/CNPJ", "Sexo"]
+# Relacionando as colunas PA_CNPJCPF e CPF_CNPJ das tabelas
+pars = pd.merge(pars, cpf_cnpj, left_on='PA_CNPJCPF', right_on='CPF_CNPJ')
 
-print(cpf_e_genero)
+print(pars['FANTASIA'].value_counts())
+
